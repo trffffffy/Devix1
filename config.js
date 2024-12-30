018bc62302f4126594e4ca93d07c87070dcb3d70
trffffffy <marwanreyad442@gmail.com>
2024-12-30 22:45:56 +0200

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,20 @@
 const GEMINI_API_KEY = 'AIzaSyB9e5yAyV3EjkS-hS-HdR2F0Yquf0Zr4JI';
 const GEMINI_API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent';
 
-export { GEMINI_API_KEY, GEMINI_API_URL };
+const AUTHORIZED_DOMAINS = [
+    'https://devix-cunaaokw6-marwanreyad442-gmailcoms-projects.vercel.app',
+    'localhost:3000',
+    'localhost:5000'
+];
+
+const API_HEADERS = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${GEMINI_API_KEY}`,
+};
+
+export { 
+    GEMINI_API_KEY, 
+    GEMINI_API_URL, 
+    AUTHORIZED_DOMAINS,
+    API_HEADERS 
+};
